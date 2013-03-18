35a3f7c65f1a654378b772ca5c120ef856e80d42
Derick Bailey <derickbailey@gmail.com>
2013-03-18 14:30:21 -0500

    added very basic kendo.backbone.DataSource implementation

--- src/web/build/kendo.backbone.js
+++ src/web/build/kendo.backbone.js
@@ -1,2 +1,8 @@
+(function(global, kendo){
+  "use strict";
+
 // @include ../kendo.backbone.model.js
 // @include ../kendo.backbone.collection.js
+// @include ../kendo.backbone.datasource.js
+ 
+})(this, kendo);
