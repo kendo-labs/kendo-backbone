94bb36bf6f48f4202b89149c6fdd07a62d018c52
Derick Bailey <derickbailey@gmail.com>
2013-03-18 14:32:28 -0500

    added empty spec file for backbone.datasource

---
+++ specs/web/kendo.backbone.datasource.js
@@ -0,0 +1,9 @@
+xdescribe("kendo.backbone.DataSource", function(){
+
+  describe("add some tests!", function(){
+    it("should have some", function(){
+      throw new Error("No tests yet! Add some!");
+    });
+  });
+
+});
