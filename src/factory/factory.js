6b0d5c7f4512b0124dde577c14d2d54c3c22cfdd
Derick Bailey <derickbailey@gmail.com>
2013-06-07 13:55:03 -0500

    strict, and some formatting

--- src/factory/factory.js
+++ src/factory/factory.js
@@ -9,14 +9,18 @@
 // outside of kendo.backbone's internal
 // uses.
 kendo.Backbone.factory = (function(){
+  "use strict";
+
   // from http://www.140byt.es/keywords/new
   function create(a,b,c){return(create[c=b?-~b.length:1]||(create[c]=Function("a,b,c","return new a("+Array(c).join(",b[c++]").slice(1)+")")))(a,b,0)}
 
   var factory = {
     types: {},
+
     register: function(name, type){
       this.types[name] = type;
     },
+
     build: function(name, args){
       var args = Array.prototype.splice.call(arguments, 1);
       return create(this.types[name], args);
