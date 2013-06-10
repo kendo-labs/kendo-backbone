2d0f954794d2d65c30f733645e1af91e1f959c52
Derick Bailey <derickbailey@gmail.com>
2013-06-10 08:33:09 -0500

    additional specs for reading datasource on collection reset

--- specs/web/datasource/collection.read.spec.js
+++ specs/web/datasource/collection.read.spec.js
@@ -26,4 +26,37 @@ describe("Kendo UI Backbone DataSource - Read Backbone Collection", function(){
       expect(dsItem.baz).toBe(item.baz);
     });
   });
+
+  describe("given a collection/datasource is not empty, when the collection is reset", function(){
+    var ds, collection, item;
+    
+    beforeEach(function(){
+      collection = new Backbone.Collection([{id: 2, name: "boo", foo: "far", baz: "books"}]);
+      ds = new kendo.Backbone.DataSource({
+        collection: collection,
+        autoSync: true
+      });
+      ds.read();
+
+      collection.on("reset", function(){
+        item = collection.at(0).toJSON();
+      });
+
+      collection.reset([{id: 1, name: "foo", foo: "bar", baz: "quux"}]);
+    });
+
+    it("should reset the datasource length to the collection length", function(){
+      expect(ds.view().length).toBe(collection.length);
+    });
+    
+    it("should have the new models' data in the DataSource", function(){
+      var dsItem = ds.view()[0];
+
+      expect(dsItem.id).toBe(item.id);
+      expect(dsItem.name).toBe(item.name);
+      expect(dsItem.foo).toBe(item.foo);
+      expect(dsItem.baz).toBe(item.baz);
+    });
+  });
+
 });
